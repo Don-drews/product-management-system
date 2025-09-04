bd11da7169b060b9e03ba7a7201ca9a12041207e
Don-drews <tsuchida2892@outlook.com>
2025-09-04 15:04:02 +0900

    descriptionのnullを許可

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,7 +1,7 @@
 export type Product = {
   id: string;
   name: string;
-  description?: string;
+  description?: string | null;
   price: number; // 円（整数）
   imageUrl: string;
   categoryId: string;
