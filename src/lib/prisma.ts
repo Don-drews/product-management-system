cb8f3b254c6f428bb491ef83eba529beaaf98100
Don-drews <tsuchida2892@outlook.com>
2025-09-05 11:39:55 +0900

    PrismaClientのログ出力に関するメモ

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,7 +7,7 @@ const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 //    あればそれを使い回す → シングルトン化
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({ log: ["query", "error", "warn"] });
+  new PrismaClient({ log: ["query", "error", "warn"] }); // オプションをつけることでログ出力される（query：指定すると、Prismaが実行した生のSQLクエリをコンソールに出力。error：エラー時にログ出る。warn：警告時にログが出る。）
 
 // 3. 本番以外の環境のときだけ global に prisma を保存
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
