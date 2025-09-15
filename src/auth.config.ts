45477516e78e21d4f31b2e7f3eaf675585ca081f
Don-drews <tsuchida2892@outlook.com>
2025-09-15 21:48:15 +0900

    auth.config.ts作成

---
+++ src/auth.config.ts
@@ -0,0 +1,12 @@
+import type { NextAuthConfig } from "next-auth";
+import Email from "next-auth/providers/email";
+
+export const authConfig = {
+  providers: [
+    Email({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+      maxAge: 60 * 60, // リンク有効期限(秒) 1時間
+    }),
+  ],
+} satisfies NextAuthConfig;
