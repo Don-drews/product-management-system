53d22e330ff48b318a250ee2a5deea1d6a0174b5
Don-drews <tsuchida2892@outlook.com>
2025-09-16 17:31:51 +0900

    めも

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,8 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "./auth.config";
 
 // 共通ヘルパー：サーバーコンポーネントやAPIで使える
+// getServerSession をラップして共通化しただけの便利関数
+// どこからでも auth() でセッション取得できる
 export function auth() {
   return getServerSession(authOptions);
 }
