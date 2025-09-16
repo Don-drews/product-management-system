18d052b2a77863bd99676f1b6acac9de65fedd91
Don-drews <tsuchida2892@outlook.com>
2025-09-16 18:09:32 +0900

    pages追加

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -18,6 +18,7 @@ export const authOptions: NextAuthOptions = {
   ],
 
   pages: {
+    signIn: "/auth/signin",
     verifyRequest: "/auth/verify-request", // verifyRequest: 「確認メールを送ったよ」画面を自作したいときに指定
     // error: "/auth/error", // 必要なら
   },
