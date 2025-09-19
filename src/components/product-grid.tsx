cb3358b9fc13c0f3bb120614b21d5793e155a9c2
Don-drews <tsuchida2892@outlook.com>
2025-09-20 03:50:09 +0900

    変数名わかりやすく

--- src/components/product-grid.tsx
+++ src/components/product-grid.tsx
@@ -11,8 +11,8 @@ export default function ProductGrid({ products }: Props) {
   // });
   return (
     <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {products.map((p) => (
-        <ProductCard key={p.id} product={p} />
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
       ))}
     </div>
   );
