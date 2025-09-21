e6bcf8489e67a627436ad1a3546f022b696df6df
Don-drews <tsuchida2892@outlook.com>
2025-09-21 18:55:26 +0900

    画像URL取得

--- src/lib/storage/url.ts
+++ src/lib/storage/url.ts
@@ -1,3 +1,7 @@
 export function getProductImageUrl(path: string) {
   return `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/products/${path}`;
 }
+
+export function getAccountImageUrl(path: string) {
+  return `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/account/${path}`;
+}
