e3d346d1e457a3563ca1f099a63f9176aee6a9e9
Don-drews <tsuchida2892@outlook.com>
2025-09-21 22:57:59 +0900

    コールバックを即反映に対応させる

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -24,11 +24,20 @@ export const authOptions: NextAuthOptions = {
   },
 
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, user, trigger, session }) {
       if (user) {
         const u = user as AdapterUser;
         token.id = u.id;
         token.role = (u.role ?? "USER") as "ADMIN" | "USER";
+        token.picture = user.image;
+      }
+      if (trigger === "update") {
+        if (typeof session?.name !== "undefined") {
+          token.name = session.name as string | null;
+        }
+        if (typeof session?.image !== "undefined") {
+          token.picture = session.image as string | null; // ← パスでもOK
+        }
       }
       return token;
     },
@@ -36,6 +45,8 @@ export const authOptions: NextAuthOptions = {
       if (session.user) {
         session.user.id = token.id as string;
         session.user.role = (token.role as "ADMIN" | "USER") ?? "USER";
+        session.user.name = token.name as string | null;
+        session.user.image = (token.picture as string | null) ?? null;
       }
       return session;
     },
