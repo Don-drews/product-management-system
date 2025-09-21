07b75a2be74efa4e9dcc4c70c1daa9e6d92ff37a
Don-drews <tsuchida2892@outlook.com>
2025-09-21 19:05:43 +0900

    アカウント画像URL取得用

--- src/lib/storage/src.ts
+++ src/lib/storage/src.ts
@@ -1,5 +1,6 @@
 import { DEFAULT_PRODUCT_IMAGE } from "@/constants/product";
 import { getProductImageUrl } from "./url";
+import { DEFAULT_ACCOUNT_IMAGE } from "@/constants/account";
 
 export function getProductImageSrcUrl(imageUrl: string) {
   // 画像URLが「フルURL」か「path」かで分岐し、最終的な表示用URLを決定
@@ -8,4 +9,13 @@ export function getProductImageSrcUrl(imageUrl: string) {
       ? imageUrl // すでにフルURLならそのまま
       : getProductImageUrl(imageUrl) // pathなら公開URLへ変換
     : DEFAULT_PRODUCT_IMAGE;
+}
+
+export function getAccountImageSrcUrl(imageUrl: string) {
+  // 画像URLが「フルURL」か「path」かで分岐し、最終的な表示用URLを決定
+  return imageUrl
+    ? /^https?:\/\//i.test(imageUrl)
+      ? imageUrl // すでにフルURLならそのまま
+      : getProductImageUrl(imageUrl) // pathなら公開URLへ変換
+    : DEFAULT_ACCOUNT_IMAGE;
 }
