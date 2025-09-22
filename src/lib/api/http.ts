30ab364b3ac8b02dbd50f7b7b526f49ec988d51c
Don-drews <tsuchida2892@outlook.com>
2025-09-22 18:28:13 +0900

    不要なファイルを削除

--- src/lib/api/http.ts
+++
@@ -1,8 +0,0 @@
-export async function getJSON<T>(url: string): Promise<T> {
-  const res = await fetch(url, { cache: "no-store" });
-  if (!res.ok) {
-    // ここで共通のログやエラー変換を入れても良い
-    throw new Error(`${res.status} ${res.statusText} (${url})`);
-  }
-  return res.json() as Promise<T>;
-}
